73cfabe2c6830db2c304454e575978bade26d35c
BugHunter-X <askzenai@gmail.com>
2024-12-31 07:36:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,25 @@
+```dart
+class MyClass {
+  int? _value;
+
+  MyClass(this._value);
+
+  int get value => _value ?? 0; // Using ?? to provide a default value
+
+  void setValue(int newValue) {
+    if (newValue >= 0) {
+      _value = newValue;
+    } else {
+      // Handle negative input appropriately.  Throwing an exception is one option.
+      throw ArgumentError('Value cannot be negative');
+    }
+  }
+}
+
+void main() {
+  var obj = MyClass(10);
+  print(obj.value); // Output: 10
+
+  obj.setValue(-5); // Throws an ArgumentError
+}
+```
